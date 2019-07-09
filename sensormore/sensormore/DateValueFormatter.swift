e4322b7be209c0717be6ce4a32a837b72b9b7df0
Adrian Wong <adrian.wong@wiivv.com>
2019-07-09 11:32:34 -0700

    sensormore initial drop

---
+++ sensormore/sensormore/DateValueFormatter.swift
@@ -0,0 +1,24 @@
+//
+//  DateValueFormatter.swift
+//  sensormore
+//
+//  Created by Adrian Wong on 2019-07-09.
+//  Copyright © 2019 Wiivv. All rights reserved.
+//
+
+import Foundation
+import Charts
+
+public class DateValueFormatter: NSObject, IAxisValueFormatter {
+    private let dateFormatter = DateFormatter()
+    
+    override init() {
+        super.init()
+        dateFormatter.dateFormat = "dd MMM HH:mm"
+    }
+    
+    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        return dateFormatter.string(from: Date(timeIntervalSince1970: value))
+    }
+}
+
---
+++ sensormore/sensormore/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  sensormore
+//
+//  Created by Adrian Wong on 2019-07-09.
+//  Copyright © 2019 Wiivv. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
